a6edfa57408f94bd12879b1f06f6d37ac6bafb6b
ritwik348 <78126537+ritwik348@users.noreply.github.com>
2021-05-17 21:05:24 -0500

    Add files via upload

---
+++ Security.js
@@ -0,0 +1,37 @@
+class Security {
+
+    constructor(){
+
+        // Add code to create the input and button elements
+        this.acsess1=createInput("Code 1 ")
+        this.acsess1.position(100,90)
+        this.acsess1.style('background', 'white')
+
+        this.button1=createButton("CHECK")
+        this.button1.position(100,120)
+        this.button1.style('background', 'white')
+
+        this.acsess2=createInput("Code 1 ")
+        this.acsess2.position(100,300)
+        this.acsess2.style('background', 'white')
+
+        this.button2=createButton("CHECK")
+        this.button2.position(100,330)
+        this.button2.style('background', 'white')
+
+        this.acsess3=createInput("Code 1 ")
+        this.acsess3.position(700,190)
+        this.acsess3.style('background', 'white')
+
+        this.button3=createButton("CHECK")
+        this.button3.position(700,220)
+        this.button3.style('background', 'white')
+    }
+
+    display(){
+
+        // Add code to make the buttons function as expected
+
+
+    }
+}
